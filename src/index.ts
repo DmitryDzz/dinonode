b0ef367eae2bd2b55923193cb0ec827b719e0148
DmitryDzz <DzakhovDS@gmail.com>
2022-03-08 14:59:45 +0300

    Yargs for the command line parameters

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,37 @@
 import {Application} from "./application";
+import yargs from "yargs";
+import {GameDifficulty, Options} from "./options";
 
 const main = (): void => {
-    // console.log("Hello Dino world!\n");
+    const argv = yargs(process.argv.slice(2))
+        .version("1.0.1")
+        .options({
+            s: {
+                alias: "maxScore",
+                type: "number",
+                default: 10,
+                description: "Score to win the game",
+            },
+            d: {
+                alias: "difficulty",
+                choices: [GameDifficulty.Easy, GameDifficulty.Medium, GameDifficulty.Hard],
+                default: GameDifficulty.Medium,
+                description: "Game difficulty"
+            },
+        })
+        .check((args) => {
+            if (isNaN(args.s) || args.s <= 0)
+                throw new Error("Wrong maxScore argument value. It should be a positive integer.");
+            return true;
+        })
+        .parseSync();
+
+    Options.maxScore = Math.floor(argv.s);
+    Options.difficulty = argv.d;
+
+    console.log(Options.maxScore);
+    console.log(Options.difficulty);
+
     const application: Application = new Application();
     application.start();
 }
--- src/options.ts
+++ src/options.ts
@@ -1,7 +1,15 @@
+export enum GameDifficulty {
+    Easy = "easy",
+    Medium = "medium",
+    Hard = "hard",
+}
+
 export class Options {
     static debug = {
         showColliders: false,
     };
 
     static maxScore: number = 99;
+
+    static difficulty: GameDifficulty = GameDifficulty.Medium;
 }
