db49053f89317a0fd9ebc134c528983fadc8004f
DmitryDzz <DzakhovDS@gmail.com>
2022-03-12 21:41:06 +0300

    Improved U.F.O. arrival

--- src/characters/ufo_phase.ts
+++ src/characters/ufo_phase.ts
@@ -30,26 +30,56 @@ export abstract class Phase {
 }
 
 export class ArrivalPhase extends Phase {
-    private static readonly ABS_SPEED = 300.0;
+    private static readonly ABS_SPEED_1 = 300.0;
+    private static readonly ABS_SPEED_2 = 200.0;
+    private static readonly ABS_SPEED_3 = 180.0;
 
-    // private _startTime?: number = undefined;
+    private static readonly DELTA_X1 = 15;
+    private static readonly DELTA_X2 = 15;
+
+    private _startTime?: number = undefined;
+    private _time1?: number = undefined;
+    private _time2?: number = undefined;
+    private _endTime?: number = undefined;
+
+    private _distanceToTarget?: float = undefined;
 
     update(): void {
         if (this._isDestroyed || this._isDone) return;
-        // if (this._startTime === undefined) this._startTime = Time.time;
-        const deltaX = Time.deltaTime * ArrivalPhase.ABS_SPEED;
-        if (this._startColumn < this._endColumn) {
-            this._x += deltaX;
-            if (this._x >= this._endColumn) {
-                this._isDone = true;
-            }
-        } else {
-            this._x -= deltaX;
-            if (this._x <= this._endColumn) {
-                this._isDone = true;
-            }
+
+        const time = Time.time;
+        if (this._startTime === undefined) {
+            this._startTime = time;
+            this._distanceToTarget = Math.abs(this._endColumn - this._startColumn);
+            this._time1 = time + (this._distanceToTarget + ArrivalPhase.DELTA_X1) / ArrivalPhase.ABS_SPEED_1;
+            this._time2 = this._time1 + (ArrivalPhase.DELTA_X1 + ArrivalPhase.DELTA_X2) / ArrivalPhase.ABS_SPEED_2;
+            this._endTime = this._time2 + ArrivalPhase.DELTA_X2 / ArrivalPhase.ABS_SPEED_3;
         }
-        if (this._isDone) {
+
+        let currentDistance: float;
+        if (time <= this._time1!) {
+            currentDistance = ArrivalPhase.ABS_SPEED_1 * (time - this._startTime);
+        } else if (time > this._time1! && time <= this._time2!) {
+            currentDistance = this._distanceToTarget! + ArrivalPhase.DELTA_X1 -
+                ArrivalPhase.ABS_SPEED_2 * (time - this._time1!);
+        } else {
+            currentDistance = this._distanceToTarget! - ArrivalPhase.DELTA_X2 +
+                ArrivalPhase.ABS_SPEED_3 * (time - this._time2!);
+        }
+
+        if (this._startColumn < this._endColumn) {
+            this._x = this._startColumn + currentDistance;
+        } else {
+            this._x = this._startColumn - currentDistance;
+        }
+
+        if (this._endTime !== undefined && time >= this._endTime) {
+            this._isDone = true;
+            this._startTime = undefined;
+            this._time1 = undefined;
+            this._time2 = undefined;
+            this._endTime = undefined;
+            this._distanceToTarget = undefined;
             this._x = this._endColumn;
             this._afterPhaseCallback();
         }
